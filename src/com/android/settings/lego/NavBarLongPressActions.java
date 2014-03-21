31f4026a52ec2950e0c031427862ee08acf8ce66
cristianomatos <cristianobmatos@gmail.com>
2014-03-21 15:41:37 -0300

    Fix Force Close of Recent long press options in settings - When Long press actions (navbar_long_press_actions.xml) is clicked first the settings look for android:key="recent_long_press" but as i'm stupid i did not rename it in NavBarLongPressActions.java This commit fix this little derp and also finally rename the remain to RECENT and not RECENTS.
    Change-Id: Ic3afd3ab4b8b4b4a88526575dc560cce1be6f2b9

--- src/com/android/settings/lego/NavBarLongPressActions.java
+++ src/com/android/settings/lego/NavBarLongPressActions.java
@@ -18,9 +18,9 @@ public class NavBarLongPressActions extends SettingsPreferenceFragment implement
         Preference.OnPreferenceChangeListener {
     private static final String TAG = "NavBarLongPressActions";
 
-    private static final String PREF_RECENTS_LONG_PRESS = "recents_long_press";
+    private static final String PREF_RECENT_LONG_PRESS = "recent_long_press";
     
-    ListPreference mRecentsLongPress;
+    ListPreference mRecentLongPress;
     
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -29,20 +29,20 @@ public class NavBarLongPressActions extends SettingsPreferenceFragment implement
         addPreferencesFromResource(R.xml.navbar_long_press_actions);
         PreferenceScreen prefScreen = getPreferenceScreen();
 
-        mRecentsLongPress = (ListPreference) findPreference(PREF_RECENTS_LONG_PRESS);
+        mRecentLongPress = (ListPreference) findPreference(PREF_RECENT_LONG_PRESS);
         int longPress = Settings.System.getIntForUser(getContentResolver(),
-                Settings.System.NAVBAR_RECENTS_LONG_PRESS, 0, UserHandle.USER_CURRENT);
-        mRecentsLongPress.setValue(String.valueOf(longPress));
-        mRecentsLongPress.setOnPreferenceChangeListener(this);
+                Settings.System.NAVBAR_RECENT_LONG_PRESS, 0, UserHandle.USER_CURRENT);
+        mRecentLongPress.setValue(String.valueOf(longPress));
+        mRecentLongPress.setOnPreferenceChangeListener(this);
         updateLongPressMode(longPress);
 
     }
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
-        if (preference == mRecentsLongPress) {
+        if (preference == mRecentLongPress) {
             int longPress = Integer.valueOf((String) objValue);
             Settings.System.putIntForUser(getContentResolver(),
-                    Settings.System.NAVBAR_RECENTS_LONG_PRESS,
+                    Settings.System.NAVBAR_RECENT_LONG_PRESS,
                     longPress, UserHandle.USER_CURRENT);
             updateLongPressMode(longPress);
             return true;
@@ -56,33 +56,33 @@ public class NavBarLongPressActions extends SettingsPreferenceFragment implement
         Resources res = getResources();
         int summary = -1;
 
-        Settings.System.putInt(cr, Settings.System.NAVBAR_RECENTS_LONG_PRESS, value);
+        Settings.System.putInt(cr, Settings.System.NAVBAR_RECENT_LONG_PRESS, value);
 
         if (value == 0) {
-            Settings.System.putInt(cr, Settings.System.NAVBAR_RECENTS_LONG_PRESS, 0);
+            Settings.System.putInt(cr, Settings.System.NAVBAR_RECENT_LONG_PRESS, 0);
             summary = R.string.recent_long_press_none;
         } else if (value == 1) {
-            Settings.System.putInt(cr, Settings.System.NAVBAR_RECENTS_LONG_PRESS, 1);
+            Settings.System.putInt(cr, Settings.System.NAVBAR_RECENT_LONG_PRESS, 1);
             summary = R.string.recent_long_press_last_app;
         } else if (value == 2) {
-            Settings.System.putInt(cr, Settings.System.NAVBAR_RECENTS_LONG_PRESS, 2);
+            Settings.System.putInt(cr, Settings.System.NAVBAR_RECENT_LONG_PRESS, 2);
             summary = R.string.recent_long_press_screenshot;
         } else if (value == 3) {
-            Settings.System.putInt(cr, Settings.System.NAVBAR_RECENTS_LONG_PRESS, 3);
+            Settings.System.putInt(cr, Settings.System.NAVBAR_RECENT_LONG_PRESS, 3);
             summary = R.string.recent_long_press_kill_app;
         } else if (value == 4) {
-            Settings.System.putInt(cr, Settings.System.NAVBAR_RECENTS_LONG_PRESS, 4);
+            Settings.System.putInt(cr, Settings.System.NAVBAR_RECENT_LONG_PRESS, 4);
             summary = R.string.recent_long_press_notif_panel;
         } else if (value == 5) {
-            Settings.System.putInt(cr, Settings.System.NAVBAR_RECENTS_LONG_PRESS, 5);
+            Settings.System.putInt(cr, Settings.System.NAVBAR_RECENT_LONG_PRESS, 5);
             summary = R.string.recent_long_press_qs_panel;
         } else if (value == 6) {
-            Settings.System.putInt(cr, Settings.System.NAVBAR_RECENTS_LONG_PRESS, 6);
+            Settings.System.putInt(cr, Settings.System.NAVBAR_RECENT_LONG_PRESS, 6);
             summary = R.string.recent_long_press_power_menu;
         }
 
-        if (mRecentsLongPress != null && summary != -1) {
-            mRecentsLongPress.setSummary(res.getString(summary));
+        if (mRecentLongPress != null && summary != -1) {
+            mRecentLongPress.setSummary(res.getString(summary));
         }
     }
 }
